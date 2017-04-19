e9a9e45548afd9ba97b94b267905990b9a3e2c51
Jai Tiwari <contactjaitiwari@gmail.com>
2017-04-19 15:54:53 -0400

    final tutorial bar chart

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-// var data = [4, 8, 15, 16, 23, 42];
+var margin = { top: 20, right: 30, bottom: 30, left: 40 };
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
-var width = 420;
-var barHeight = 20;
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 
-var x = d3.scaleLinear()
-  .range([0, width])
+var y = d3.scale.linear().range([height, 0]);
+
+var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient('bottom')
+  ;
+
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient('left')
+  .ticks(10, '%')
   ;
 
 var chart = d3.select('.chart')
-  .attr('width', width)
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
   ;
 
-d3.tsv('data.tsv', type, function (error, data) {
-  x.domain([0, d3.max(data, function (d) { return d.value; })]);
-  chart.attr('height', barHeight * data.length);
+d3.tsv('data.tsv', type, (error, data) => {
 
-  var bar = chart.selectAll('g')
+  x.domain(data.map(d => d.name));
+  y.domain([0, d3.max(data.map(d => d.value))]);
+
+  chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy','.71em')
+    .style('text-anchor', 'end')
+    .text('Frequency')
+    ;
+
+  chart.selectAll('.bar')
     .data(data)
-    .enter().append('g')
-    .attr('transform', (d, i) => "translate(0," + i * barHeight + ")");
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.value))
+    .attr('width', x.rangeBand())
+    .attr('height', d => height - y(d.value))
+    ;
 
-  bar.append('rect')
-    .attr('width', d => x(d.value))
-    .attr('height', barHeight - 1);
-
-  bar.append("text")
-    .attr("x", function (d) { return x(d.value) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function (d) { return d.value; });
 });
+
+
 
 function type(d) {
   d.value = +d.value;
